c062706fd1f575fcfcadb7644cc8e5f64142fce5
Mellio <kllmmc23@gmail.com>
2023-04-09 15:29:52 -0600

    add cdk-s3-ts

---
+++ cdk-s3-ts/lib/cdk-s3-ts-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkS3TsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // s3 bucket
+    const bucket = new s3.Bucket(this, "CdkS3Ts", {
+      bucketName: "cdk-s3-ts-04092023",
+      versioned: false,
+      publicReadAccess: false,
+    });
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkS3TsQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
