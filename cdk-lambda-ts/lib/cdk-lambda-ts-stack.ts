49274931750f495781317fa9bb44822978fdb442
Mellio <kllmmc23@gmail.com>
2023-04-11 22:21:24 -0600

    add cloudwatch alarm for errors

--- cdk-lambda-ts/lib/cdk-lambda-ts-stack.ts
+++ cdk-lambda-ts/lib/cdk-lambda-ts-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
 
 export class CdkLambdaTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,9 +12,16 @@ export class CdkLambdaTsStack extends cdk.Stack {
     //create lambda function
     const lbfn = new lambda.Function(this, "lambdaid", {
       handler: "lambda_function.lambda_handler",
-      runtime: lambda.Runtime.PYTHON_3_9, 
-      code: lambda.Code.fromAsset(path.join(__dirname, '../services')),
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset(path.join(__dirname, "../services")),
       functionName: "cdk-lambda",
+    });
+
+    //add cloudwatch alarm
+    const alarm = new cloudwatch.Alarm(this, "Alarm", {
+      metric: lbfn.metricErrors(),
+      threshold: 1,
+      evaluationPeriods: 1,
     });
   }
 }
