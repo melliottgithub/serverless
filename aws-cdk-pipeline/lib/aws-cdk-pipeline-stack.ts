b2501387f9d625f8d536c24c4f950edb6346adfe
Mellio <kllmmc23@gmail.com>
2023-04-13 23:05:46 -0600

    add codepipeline test run

--- aws-cdk-pipeline/lib/aws-cdk-pipeline-stack.ts
+++ aws-cdk-pipeline/lib/aws-cdk-pipeline-stack.ts
@@ -10,13 +10,15 @@ export class AwsCdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // aws ci cd pipeline
+    // aws ci cd pipeline and add oath token const pipeline = new CodePipeline(this, "Pipeline", {
+
     const pipeline = new CodePipeline(this, "Pipeline", {
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub(
-          "melliottgithub/serverless/aws-cdk-pipeline",
-          "main"
-        ),
+        input: CodePipelineSource.gitHub("melliottgithub/serverless", "main", {
+          authentication: cdk.SecretValue.secretsManager("github-token", {
+            jsonField: "github-token",
+          }),
+        }),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
