8ef58f3e8c39a93dd5a536777ec6354cd856b51e
Mellio <kllmmc23@gmail.com>
2023-04-13 21:30:14 -0600

    add codepipeline

---
+++ aws-cdk-pipeline/lib/aws-cdk-pipeline-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+//codepipeline
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+export class AwsCdkPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // aws ci cd pipeline
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "",
+          "main"
+        ),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
+  }
+}
