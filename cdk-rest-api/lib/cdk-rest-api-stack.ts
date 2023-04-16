b6c595989065c0cbeb1af41f0e7a30222c58f502
Mellio <kllmmc23@gmail.com>
2023-04-15 19:06:23 -0600

    add gateway resources and methods

--- cdk-rest-api/lib/cdk-rest-api-stack.ts
+++ cdk-rest-api/lib/cdk-rest-api-stack.ts
@@ -10,7 +10,7 @@ export class CdkRestApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     // dynamodb table
-    const factsTable = new Table(this, "factsTable", {
+    const factTable = new Table(this, "FactTable", {
       partitionKey: { name: "id", type: AttributeType.STRING },
       billingMode: BillingMode.PAY_PER_REQUEST,
       tableName: "factsTable",
@@ -18,31 +18,32 @@ export class CdkRestApiStack extends cdk.Stack {
     });
 
     // lambda function
-    const handlerFn = new Function(this, "getfactslogicalid", {
+    const factHandler = new Function(this, "GetFactsHandler", {
       functionName: "get-facts",
       runtime: Runtime.PYTHON_3_9,
       code: Code.fromAsset(join(__dirname, "../package.zip")),
       handler: "app.lambda_handler",
       environment: {
-        TABLE_NAME: factsTable.tableName,
+        TABLE_NAME: factTable.tableName,
       },
     });
     //grant lambda function to read from dynamodb table
-    factsTable.grantReadWriteData(handlerFn);
+    factTable.grantReadWriteData(factHandler);
     // api gateway rest api
-    const api = new RestApi(this, "getfactsapi", {
+    const api = new RestApi(this, "FactApiGateway", {
       description: "API Gateway for getting facts",
-      restApiName: "get-facts-api",
+      restApiName: "FactApiGateway",
     });
     // api gateway resource
-    const facts = api.root.addResource("getfacts");
+    const factsResource = api.root.addResource("facts");
+    const factResource = api.root.addResource("{factId}");
     // api gateway integration
-    const getFactsIntegration = new LambdaIntegration(handlerFn);
+    const getFactsIntegration = new LambdaIntegration(factHandler);
     // api gateway method
-    facts.addMethod("GET", getFactsIntegration);
-    facts.addMethod("POST", getFactsIntegration);
-    facts.addMethod("PUT", getFactsIntegration);
-    facts.addMethod("DELETE", getFactsIntegration);
-    
+    factsResource.addMethod("GET", getFactsIntegration);
+    factsResource.addMethod("POST", getFactsIntegration);
+    factResource.addMethod("GET", getFactsIntegration);
+    factResource.addMethod("PUT", getFactsIntegration);
+    factResource.addMethod("DELETE", getFactsIntegration);
   }
 }
