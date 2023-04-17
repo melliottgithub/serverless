b76f12b4d59d3deb8d2b89a880ec0769f409ec16
Mellio <kllmmc23@gmail.com>
2023-04-16 20:06:27 -0600

    add cron job

--- cron-job/lib/cron-job-stack.ts
+++ cron-job/lib/cron-job-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Runtime, Function, Code } from "aws-cdk-lib/aws-lambda";
 
-export class CronJobStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CronJobStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CronJobQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // add lambnda function
+    const lambda = new Function(this, "CronJob", {
+      runtime: Runtime.PYTHON_3_9,
+      handler: "index.handler",
+      code: Code.fromAsset("lambda"),
+      functionName: "CronJob",
+      description: "Cron Job",
+    });
   }
 }
